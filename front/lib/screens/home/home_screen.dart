e7a3ab5cfe834a29265ba4cfc40e664cc1a85a47
ianisparfait <parfait.ianis@gmail.com>
2025-04-08 19:04:31 +0200

    error msg

--- front/lib/screens/home/home_screen.dart
+++ front/lib/screens/home/home_screen.dart
@@ -1,8 +1,5 @@
-import "package:booking/model/booking.dart";
 import "package:flutter/material.dart";
 import "package:booking/api/api.dart";
-import "package:booking/model/response.dart";
-import "package:booking/widgets/screen_container.dart";
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -36,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
    } catch (e) {
      ScaffoldMessenger.of(context).showSnackBar(
        SnackBar(
-         content: Text("Erreur lors de la récupération des données: $e"),
+         content: Text("Error while fetching data: $e"),
          duration: const Duration(seconds: 3),
        ),
      );
